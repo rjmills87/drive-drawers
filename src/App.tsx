92f2df42a1449021ea22fa548292f59d712820e6
Rhys Miller <rjmills87@aol.com>
2025-05-23 22:29:03 +0100

    refactor: reorder imports and clean up comments in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import { useState } from "react";
+import { getFilesForPath } from "./data/mockData";
 import Header from "./components/Header";
 import ServiceSelector from "./components/ServiceSelector";
 import ActionBar from "./components/ActionBar";
 import FileList from "./components/FileList";
 import Loading from "./components/Loading";
 import Breadcrumb from "./components/Breadcrumb";
-import { getFilesForPath } from "./data/mockData";
 
 function App() {
   // Loading state
   const [isLoading] = useState<boolean>(false);
 
-  // Path State
+  // File Path State
   const [currentPath, setCurrentPath] = useState<string>("");
 
   const navigateToFolder = (folderName: string) => {
@@ -25,9 +25,7 @@ function App() {
 
   return (
     <div className="w-[400px] h-[500px] flex flex-col bg-gray-50">
-      {/* Header */}
       <Header />
-      {/* Service Selection */}
       <ServiceSelector />
       {/* Main Content Area */}
       <div className="flex-1 overflow-y-auto p-4">
@@ -43,7 +41,6 @@ function App() {
           </div>
         )}
       </div>
-      {/* Footer/Action Bar */}
       <ActionBar />
     </div>
   );
