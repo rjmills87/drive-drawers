e185a64e5531f56991b5e282fe934f8eefd1a5ea
Rhys Miller <rjmills87@aol.com>
2025-05-25 22:52:50 +0100

    feat: implement Google Drive authentication service and storage constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+const StorageKeys = {
+  AUTH_TOKEN: "auth_token",
+  OPTIONS: "options",
+  CURRENT_SERVICE: "current_service",
+  CURRENT_PATH: "current_path",
+};
+
+const CloudService = {
+  GOOGLE_DRIVE: "google-drive",
+  MICROSOFT_ONEDRIVE: "microsoft-onedrive",
+  DROPBOX: "dropbox",
+};
+
+export { StorageKeys, CloudService };
