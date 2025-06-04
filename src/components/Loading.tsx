c46367a2142b1e3df6b433f60d0c9cd1a4fe46ac
Rhys Miller <rjmills87@aol.com>
2025-06-04 22:25:40 +0100

    feat: replace text loading indicator with animated SVG spinner and improve modal layout

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,18 @@
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="text-gray-500">Loading...</div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="spinner-1 w-10 h-10 shrink-0 animate-spin"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12.001 5.04a2.32 2.32 0 1 0 0-4.64 2.32 2.32 0 0 0 0 4.64zm0 18.56a2.32 2.32 0 1 0 0-4.64 2.32 2.32 0 0 0 0 4.64zm9.197-14.23a2.32 2.32 0 1 1-2.32-4.02 2.32 2.32 0 0 1 2.32 4.02zM1.956 17.8a2.32 2.32 0 1 0 4.018-2.32 2.32 2.32 0 0 0-4.018 2.32zm16.922.85a2.32 2.32 0 1 1 2.32-4.02 2.32 2.32 0 0 1-2.32 4.02zM1.956 6.2a2.32 2.32 0 1 0 4.018 2.32A2.32 2.32 0 0 0 1.956 6.2z"
+          clip-rule="evenodd"
+          data-original="#000000"
+        />
+      </svg>
     </div>
   );
 }
